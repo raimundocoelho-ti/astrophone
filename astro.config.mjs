36ff34de71aa7cb5460d550804728ee437f68e89
Raimundo Coelho TI <raimundocoelho.ti@gmail.com>
2025-07-12 20:15:33 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
